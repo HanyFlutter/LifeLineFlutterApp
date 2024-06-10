dfee724d9dae48732e515ce8fb56c1f43a86d408
Hany <al7esab001@gmail.com>
2024-06-11 00:11:27 +0300

    update new screen

--- lib/modules/home_page_screen/hospital_home_page_screen/hospital_list_of_screens.dart
+++ lib/modules/home_page_screen/hospital_home_page_screen/hospital_list_of_screens.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lifeline/modules/home_page_screen/hospital_home_page_screen/heros_screen/heros_screen_hospital.dart';
 import 'package:lifeline/modules/home_page_screen/hospital_home_page_screen/hospital_argent_call/hospital_argent_call.dart';
 import 'package:lifeline/modules/home_page_screen/hospital_home_page_screen/hospital_blood_drives/hospital_blood_drive.dart';
 import 'package:lifeline/modules/home_page_screen/hospital_home_page_screen/hospital_orders_list/hospital_orders_list.dart';
